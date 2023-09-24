29a2da1dca561d7ec352adc23aa0d05c88e430c4
adenning4 <41348927+adenning4@users.noreply.github.com>
2023-09-24 14:12:35 -0600

    Database progress

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
+
 const endorsementInputEl = document.getElementById("endorsement-input");
 const fromInputEl = document.getElementById("from-input");
 const toInputEl = document.getElementById("to-input");
 const publishButtonEl = document.getElementById("publish-button");
 
+const firebaseConfig = {
+  databaseURL: "https://endorsementscrim-default-rtdb.firebaseio.com",
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+const dbEndorsementsRef = ref(database, "endorsements");
+
 publishButtonEl.addEventListener("click", () => {
   if (isAllInputsFilled()) {
+    const fromText = fromInputEl.value;
+    const endorsementText = endorsementInputEl.value;
+    const toText = toInputEl.value;
+
     clearAllInputs();
+
+    const newEndorsement = {
+      fromText,
+      endorsementText,
+      toText,
+    };
+
+    push(dbEndorsementsRef, newEndorsement);
   }
+});
+
+onValue(dbEndorsementsRef, (snapshot) => {
+  console.log(snapshot);
 });
 
 function isAllInputsFilled() {
@@ -19,4 +53,15 @@ function clearAllInputs() {
   fromInputEl.value = "";
 }
 
-const endorsementTemplateHtml = "";
+const endorsementTemplateHtml = `
+    <div class="endorsement">
+        <div class="from-tag top">To PLACEHOLDER</div>
+        <div class="endorsement-text middle">
+        PLACEHOLDER
+        </div>
+        <div class="bottom">
+        <div class="to-tag">From PLACEHOLDER</div>
+        <div class="likes"><i class="fa-solid fa-heart"></i> PLACEHOLDER</div>
+        </div>
+    </div>
+`;
