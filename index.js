4a5b94e902152ae14bb7890ba65648838c96898c
adenning4 <41348927+adenning4@users.noreply.github.com>
2023-09-24 22:04:14 -0600

    Complete Draft
    - All endorsements appear from db
    - Like button functionality
    - First 'complete' draft

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ import {
   ref,
   push,
   onValue,
+  update,
 } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
 
 const endorsementInputEl = document.getElementById("endorsement-input");
 const fromInputEl = document.getElementById("from-input");
 const toInputEl = document.getElementById("to-input");
 const publishButtonEl = document.getElementById("publish-button");
+const endorsementsContainerEl = document.getElementById(
+  "endorsements-container"
+);
 
 const firebaseConfig = {
   databaseURL: "https://endorsementscrim-default-rtdb.firebaseio.com",
@@ -20,6 +24,26 @@ const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
 const dbEndorsementsRef = ref(database, "endorsements");
+
+let dbLocalCopy;
+onValue(dbEndorsementsRef, (snapshot) => {
+  if (snapshot.exists()) {
+    const dbEndorsements = Object.entries(snapshot.val());
+    dbLocalCopy = snapshot.val();
+    console.log(dbLocalCopy);
+
+    endorsementsContainerEl.innerHTML = "";
+
+    for (let entry of dbEndorsements) {
+      const entryData = {
+        id: entry[0],
+        ...entry[1],
+      };
+
+      endorsementsContainerEl.innerHTML += getEndorsementHtml(entryData);
+    }
+  }
+});
 
 publishButtonEl.addEventListener("click", () => {
   if (isAllInputsFilled()) {
@@ -33,15 +57,37 @@ publishButtonEl.addEventListener("click", () => {
       fromText,
       endorsementText,
       toText,
+      isLiked: false,
+      likes: 0,
     };
 
     push(dbEndorsementsRef, newEndorsement);
   }
 });
 
-onValue(dbEndorsementsRef, (snapshot) => {
-  console.log(snapshot);
+document.addEventListener("click", (e) => {
+  if (e.target.dataset.id) {
+    handleLikeClick(e.target.dataset.id);
+  }
+  if (e.target.parentElement.dataset.id) {
+    handleLikeClick(e.target.parentElement.dataset.id);
+  }
 });
+
+// Assuming only one user in this small project case
+function handleLikeClick(id) {
+  const isAlreadyLiked = dbLocalCopy[id].isLiked;
+  const numberOfLikes = isAlreadyLiked
+    ? dbLocalCopy[id].likes - 1
+    : dbLocalCopy[id].likes + 1;
+
+  const updateRef = ref(database, `endorsements/${id}`);
+  update(updateRef, {
+    ...dbLocalCopy[id],
+    isLiked: !dbLocalCopy[id].isLiked,
+    likes: numberOfLikes,
+  });
+}
 
 function isAllInputsFilled() {
   return !!endorsementInputEl.value && !!toInputEl.value && !!fromInputEl.value;
@@ -53,15 +99,26 @@ function clearAllInputs() {
   fromInputEl.value = "";
 }
 
-const endorsementTemplateHtml = `
-    <div class="endorsement">
-        <div class="from-tag top">To PLACEHOLDER</div>
-        <div class="endorsement-text middle">
-        PLACEHOLDER
+function getEndorsementHtml({
+  toText,
+  endorsementText,
+  fromText,
+  id,
+  isLiked,
+  likes,
+}) {
+  return `
+        <div class="endorsement">
+            <div class="from-tag top">To ${toText}</div>
+            <div class="endorsement-text middle">
+            ${endorsementText}
+            </div>
+            <div class="bottom">
+            <div class=ds"to-tag">From ${fromText}</div>
+            <div class="likes" data-id='${id}'><i class="fa-${
+    isLiked ? "solid" : "regular"
+  } fa-heart"></i> ${likes}</div>
+            </div>
         </div>
-        <div class="bottom">
-        <div class="to-tag">From PLACEHOLDER</div>
-        <div class="likes"><i class="fa-solid fa-heart"></i> PLACEHOLDER</div>
-        </div>
-    </div>
-`;
+    `;
+}
